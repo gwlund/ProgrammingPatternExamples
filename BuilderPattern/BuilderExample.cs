2b1a803842038fed6c5c48f71c42442b6de5545a
Gil Lund <gwlund71@gmail.com>
2019-08-12 17:36:41 -0700

    Added another builder example

--- BuilderPattern/BuilderExample.cs
+++ BuilderPattern/BuilderExample.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http.Headers;
 using System.Runtime.InteropServices.WindowsRuntime;
 
@@ -16,7 +17,54 @@ namespace ProgrammingPatternExamples
             Console.WriteLine("Product Name: {0}", product.Name );
             Console.WriteLine("Product Initial Value {0}", product.InitialValue);
 
+            //Example #2 - https://learning.oreilly.com/library/view/design-patterns-in/9781484243664/html/476082_1_En_3_Chapter.xhtml
+            HtmlElement root = HtmlElement
+                .Create("ul")
+                .AddChildFluent("li", "hello")
+                .AddChildFluent("li", "world");
+
+            Console.WriteLine(root);
+
         }
+    }
+
+    public class HtmlElement
+    {
+        protected string Name, Text;
+
+        protected List<HtmlElement> Elements = new List<HtmlElement>();
+        protected const int indentSize = 2;  
+        
+        // hide the constructors!
+        protected HtmlElement() {}
+        protected HtmlElement(string name, string text)  {    Name = name;    Text = text;  }  
+        
+        // factory method
+        public static HtmlBuilder Create(string name) => new HtmlBuilder(name);
+
+    }
+
+    public class HtmlBuilder
+    {
+        protected readonly string rootName;
+        protected HtmlElement root = new HtmlElement();
+
+        public static implicit operator HtmlElement(HtmlBuilder builder)
+        {
+            return builder.root;
+        }
+
+        public HtmlBuilder(string rootName) { this.rootName = rootName; root.Name = rootName; }
+
+        public HtmlBuilder AddChildFluent(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            root.Elements.Add(e);
+            return this;
+        }
+
+        public override string ToString() => root.ToString();
+
     }
 
     public interface IBuilder
---
+++ NullableExamples/NullableExamples.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace ProgrammingPatternExamples
+{
+    public class NullableExamples
+    {
+        public void Run()
+        {
+            Customer customer = new Customer("Customer Name");
+
+            //If customer.Address is null, the expression customer.Address?.Country won’t try to
+            //evaluate the Country property, and the result of the expression will be null.
+            //The null coalescing operator then provides a default value to print.
+            Console.WriteLine(customer.Address?.Country ?? "(Address unknown)");
+
+        }
+    }
+
+    public class Customer
+    {
+
+        public Customer(string name) => Name = name;
+        public string Name { get; set; }
+        public Address? Address { get; set; }
+    }
+
+    public class Address
+    {
+        public string Country { get; set; }
+    }
+}
